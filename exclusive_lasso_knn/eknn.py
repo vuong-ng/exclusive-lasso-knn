a5f08f5370e79aca7ec60abf462e2ffc2cafab2b
vuong-ng <109729665+vuong-ng@users.noreply.github.com>
2024-06-05 13:51:28 -0400

    added files that failed to load

---
+++ exclusive_lasso_knn/eknn.py
@@ -0,0 +1,323 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os # not used so far
+import pandas as pd
+import scipy.io
+from sklearn import linear_model
+from sklearn import model_selection
+from scipy.optimize import minimize
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
+
+
+class exclusive_lasso:
+    def __init__(self, X, y,  groups_vect, lambda_):
+        self.X = X # predictors pxn matrix
+        self.y = y # test sample pxn_test
+        self.groups_vect = groups_vect
+        self.lambda_ = lambda_ # cross_validate then pass into the function
+
+
+    def regularized_least_square(self,alphas):
+        """calculate the residual of X*/alphas compared to the test sample y
+
+        Args:
+            alphas (nx1 vector): coefficients vector
+
+        Returns:
+            integer: residual between X*/alphas and y
+        """
+        residual = self.y - (self.X @ alphas)
+        # print(self.groups_vect.shape, "\n", alphas.shape)
+        grouped_alphas = self.groups_vect @ np.absolute((alphas)) 
+        return (np.sum(residual ** 2) + self.lambda_ * (np.sum(grouped_alphas ** 2))) # Square root of residuals plus the lasso penalty
+
+    def lasso_optimize(self, x0):
+        """argmin on the least squares to find optimal sparse alphas
+
+        Args:
+            x0 (float): nx1 vector initial values of alphas
+
+        Returns:
+            optimal alphas (as sklearn object) : nx1 optimal vector of coefficients
+        """
+        func = lambda x: self.regularized_least_square(x)
+        
+        return minimize(func, x0, method='SLSQP')
+
+
+class EkNN_C:
+    def __init__(self, X, coefs_vect, x_labels, k):
+        self.coefs_vect = coefs_vect # result from lasso_optimize function
+        self.k = k
+        self.x_labels = x_labels
+        self.X = X # predict = pxn matrix
+        
+        # generate k largest coeffcients for the model
+        coefs = self.coefs_vect
+        coefs = coefs.tolist()
+        coefs.sort(reverse=True)
+        max_coefs = coefs[:self.k]
+        k_largest_coefs_vect = self.coefs_vect
+
+        for j in range(len(self.coefs_vect)):
+            if k_largest_coefs_vect[j] not in max_coefs:
+                k_largest_coefs_vect[j] = 0
+                
+        self.k_largest_coefs_vect  = k_largest_coefs_vect
+    
+    def class_coefs_vect(self, label_index):
+        """represent coefficients according to a particular class labels 
+
+        Args:
+            label_index (label of a particular class): represent using integer values
+            k_largest_coefs (float): nx1 sparse vector containing only k largest optimal coefficients
+        """
+        c_coefs = np.zeros(self.X.shape[1])
+        for i in range(len(self.x_labels[label_index])):
+            if self.x_labels[label_index,i] == 1:
+                c_coefs[i] = self.k_largest_coefs_vect[i]
+            else:
+                c_coefs[i] = 0
+        return c_coefs
+    
+        
+    def predict(self):
+        # choose k largest neighbor coeficients
+        # k_largest_coefs_vect = self.k_largest_coefs_vect
+        # k_largest_coefs vector represents only k largest coeficients
+        
+        # build coeficient vector for each class. Coeficients that
+        ## are not k largest neighbors will be 0
+        
+        class_coefs = [] # list of m coeficient vector represent m classes
+        for i in range(len(self.x_labels)):
+            class_coefs.append(self.class_coefs_vect(i))
+        
+        coefs_sums = [np.sum(class_coefs[i]) for i in range(len(class_coefs))]
+        largest_coefs = max(coefs_sums)
+        
+        return coefs_sums.index(largest_coefs)
+            
+        
+        #calculate L1-norm of coeficient vector
+        
+class EkNN_R:
+    def __init__(self, X, y, coefs_vect, x_labels, k):
+        self.coefs_vect = coefs_vect
+        self.k = k
+        self.x_labels = x_labels # mxn matrix that represent groups of each observation by 0,1
+        self.X = X # predictors matrix pxn 
+        self.y = y # test sample matrix pxn_test
+        
+        # construct nx1 vector containing k non-zero coeficients
+        # which are k largest coefficients
+        # Other coefficients that are not one of the k largest remains 0
+        coefs = self.coefs_vect
+        coefs = coefs.tolist()
+        coefs.sort(reverse=True)
+        max_coefs = coefs[:self.k]
+        k_largest_coefs = self.coefs_vect
+
+        for j in range(len(self.coefs_vect)):
+            if k_largest_coefs[j] not in max_coefs:
+                k_largest_coefs[j] = 0
+                
+        self.k_largest_coefs_vect  = k_largest_coefs
+    
+    def class_coefs_vect(self, label_index):
+        """represent coeficients according to a particular class labels 
+
+        Args:
+            label_index (_type_): _description_
+            k_largest_coefs (_type_): vector containing only k largest optimal coeficients
+        """
+        
+        c_coefs = np.zeros(self.X.shape[1])
+        for i in range(self.X.shape[1]): # columns index = index of observations
+            if self.x_labels[label_index,i] == 1:
+                c_coefs[i] = self.k_largest_coefs_vect[i]
+            else:
+                c_coefs[i] = 0
+        return c_coefs # this can just be a list
+    
+
+    def create_obs_vects(self):
+        """helper function to generate vector for each training data (each observation)
+            to calculate the distance later.
+
+        Returns:
+            _list_: list containing n nx1 vectors representing n observations in the 
+            training set
+        """
+        x_vects = []
+        
+        p = self.X.shape[0] # number of unknowns
+        
+        for obsv in range(self.X.shape[1]):
+            res = np.zeros(p)
+            for i in range(p):
+                res[i] = self.X[i,obsv]
+            x_vects.append(res)
+        
+        return x_vects
+
+    def distance(self):
+        """calculate distance from the test sample y to k nearest neighbors
+
+        Args:
+            self.x (_float_): nx1 vector representing 1 observation in the training set
+            self.alphas (_float_): coefficients vector
+
+        Returns:
+            _float_: nx1 vector represents contains k non-zero distance value from 
+            y to k nearest neighbors
+        """
+        x_vects = self.create_obs_vects()
+        d_vect = np.zeros(self.X.shape[1]) # number of observations
+        
+        for i in range(self.X.shape[1]):
+            if self.k_largest_coefs_vect[i] == 0:
+                d_vect[i] = 0 # is it safe to set those not k largest to 0
+            else: 
+                d_vect[i] = np.linalg.norm(self.y - self.k_largest_coefs_vect[i] * x_vects[i], ord=2)
+                
+        return d_vect
+    
+    def weights(self):
+        """returns the weight corresponding to the distance of each observation to y
+
+        Returns:
+            _float_: nx1 vector shows weights of n observations to y
+        """
+        
+        weights = np.zeros(self.X.shape[1])
+        d_vect = self.distance()
+        
+        for i in range(self.X.shape[1]):
+            if self.k_largest_coefs_vect[i]==0:
+                weights[i] = -1
+            elif np.amax(d_vect) != np.amin(d_vect):
+                weights[i] = ((np.amax(d_vect) - d_vect[i])/(np.amax(d_vect)-np.amin(d_vect)))
+            else:
+                weights[i] = 1
+        return weights
+    
+    def predict(self):
+        """returns y's label according to 
+
+        Returns:
+            _integer_: integer encoding predicted class label of y (l_y)
+        """
+        W = self.weights()
+        class_coefs_mat = np.zeros((self.x_labels.shape[0], self.x_labels.shape[1]))
+        # class_coefs_list = []
+        
+        for i in range(self.x_labels.shape[0]):
+            class_coefs_mat[i,:] = (self.class_coefs_vect(i))
+        
+        sum_coefs_weights = class_coefs_mat @ W
+        # return W
+        return np.where(sum_coefs_weights==max(sum_coefs_weights))[0][0]
+
+class ExclusiveLassoKNNClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, lambda_=1.0, group_num=1, k=3):
+        self.lambda_ = lambda_
+        self.group_num = group_num
+        self.k = k
+    
+    def fit(self, X, x_labels):
+        
+        # X passed in have nxp dimension
+        self.group_vect = self.group_encode(X) 
+        # print ("here: ",self.group_vect.shape)
+        self.X = np.transpose(X) # X_train
+        # self.x_labels = x_labels # y_train 
+    
+        class_names = [i for i in range(len(np.unique(x_labels)))]
+        
+        self.x_labels = self.label_matrix(class_names, x_labels)
+
+        self.is_fitted_ = True
+
+        return self
+    
+    def predict(self, Y):
+        # Y test sample
+        preds = np.zeros(Y.shape[1])
+        
+        # modify y's shape
+        y_s = np.transpose(Y)
+        # print(self.X.shape)
+        for i in range(y_s.shape[0]):
+            # xL2 = np.random.rand((self.X.shape[1]))
+            xL2 = np.linalg.pinv(self.X) @ y_s[i]
+            reg = exclusive_lasso(self.X, y_s[i], self.group_vect, self.lambda_)
+            coefs = reg.lasso_optimize(xL2).x
+            # print(reg.lasso_optimize(xL2).message)
+            knn_R = EkNN_R(self.X, y_s[i], coefs, self.x_labels, self.k)
+            preds[i]=(knn_R.predict())
+
+        return preds
+    
+    
+    def label_matrix(self,label_names:list, labels):
+        class_num = len(label_names)
+        n_obs = labels.shape[0]
+        label_mat = np.zeros((class_num, n_obs))
+        for i in range(class_num):
+            for ind,j in enumerate(labels):
+                if j == label_names[i]:
+                    label_mat[i,ind] = 1
+                else:
+                    label_mat[i,ind] = 0
+        return label_mat
+    
+    
+    def group_encode(self, X):
+        # call before having X
+        # n = X.shape[1]
+        # group_pop = int(n/self.group_num)
+        # group_vect = []
+        # start = 0
+        # end = group_pop
+        # while end <= n:
+        #     temp = np.zeros(X.shape[1])
+        #     if end + group_pop > n:
+        #         temp[start:n] = 1
+        #         group_vect.append(temp)
+        #         break
+        #     else: temp[start:end] = 1
+        #     start = end
+        #     end += group_pop
+        #     group_vect.append(temp)
+        # return np.array(group_vect)
+    
+        # 2nd approach perform unsupervised learning
+        kmeans = KMeans(n_clusters=self.group_num, random_state=0, n_init="auto").fit(X)
+        result = kmeans.labels_
+        group_vect = np.zeros((self.group_num, len(result)))
+        for i in range(self.group_num):
+            for j in range(len(result)):
+                if result[j] == i:
+                    # print(result[j],"and ",i )
+                    group_vect[i,j] = 1
+        # print (group_vect.shape)
+        return group_vect
+
+
+
+    def score(self, Y, y_labels):
+        """
+        Calculate the accuracy of the classifier.
+        """
+        y_pred = self.predict(Y)
+        return accuracy_score(y_labels, y_pred)  
+            
+        
+        
+        
---
+++ exclusive_lasso_knn/eknn_poly.py
@@ -0,0 +1,285 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import scipy.io
+from sklearn import linear_model
+from sklearn import model_selection
+from scipy.optimize import minimize
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
+
+class polynomial_reg:
+    def __init__(self, X, y, groups_vect, lambda_):
+        self.X = X
+        self.y = y
+        self.groups_vect = groups_vect
+        # self.lambda_1 = lambda_1
+        self.lambda_ = lambda_
+        
+    def regularized_least_square(self, coefficients):
+        
+        # coefficients vector is alphas stacked upon betas
+        alphas = coefficients[0:self.X.shape[1]]
+        betas = coefficients[self.X.shape[1]:]
+        grouped_alphas = self.groups_vect @ np.absolute(alphas)
+        grouped_betas = self.groups_vect @ np.absolute(betas)
+        residual= self.y + self.y**2 - (self.X @ alphas + self.X**2 @ betas)
+        return (np.sum(residual**2) + self.lambda_ * (np.sum(grouped_alphas ** 2)) + self.lambda_ * (np.sum(grouped_betas ** 2)) )
+    
+    def lasso_optimize(self, x0):
+        func = lambda stacked_coefficients: self.regularized_least_square(stacked_coefficients)
+        return minimize(func, x0, method='SLSQP')
+    
+class polynomial_EkNN_C:
+    def __init__(self, X, alphas, betas, x_labels_encode, k):
+        self.alphas = alphas 
+        self.betas = betas
+        self.k = k
+        self.X = X
+        self.x_labels_encode = x_labels_encode # labels encoding
+        
+        # generate k largest coefficients for the model
+        sum_coefs = alphas + betas
+        sum_coefs_list = sum_coefs.tolist()
+        sum_coefs_list.sort(reverse=True)
+        max_sum_coefs_list = sum_coefs_list[:self.k]
+        
+        k_largest_sum_coefs = alphas + betas
+        
+        for j in range(len(sum_coefs_list)):
+            if k_largest_sum_coefs[j] not in max_sum_coefs_list:
+                k_largest_sum_coefs[j] = 0
+        
+        for i in range(len(sum_coefs_list)):
+            if k_largest_sum_coefs[i] == 0:
+                alphas[i] = 0
+                betas[i] = 0
+        
+        self.largest_alphas = alphas
+        self.largest_betas = betas
+        self.k_largest_sum_coefs = k_largest_sum_coefs
+        
+    def class_coefs_vect(self):
+        
+        class_coefs_vect = np.zeros((self.x_labels_encode.shape[0],self.X.shape[1])) # m classes x n number of observations 
+        
+        for i in range(len(self.x_labels_encode)):
+            for j in range(self.X.shape[1]):
+                if self.x_labels_encode[i, j] == 1:
+                    class_coefs_vect[i, j] = self.k_largest_sum_coefs[j]
+                else:
+                    class_coefs_vect[i, i] = 0
+        return class_coefs_vect
+        
+        # class_alphas_vect = np.zeros((len(self.x_labels_encode.shape[0]),self.X.shape[1])) # number of observations 
+        
+        # for i in range(len(self.x_labels_encode)):
+        #     for j in range(len(self.X.shape[1])):
+        #         if self.x_labels_encode[i, j] == 1:
+        #             class_alphas_vect[i, j] = self.largest_alphas[j]
+        #         else:
+        #             class_alphas_vect[i, i] = 0
+                    
+        # class_betas_vect = np.zeros((len(self.x_labels_encode.shape[0]), self.X.shape[1]))
+        
+        # for i in range(len(self.x_labels_encode)):
+        #     for j in range(len(self.X.shape[1])):
+        #         if self.x_labels_encode[i,j] == 1:
+        #             class_betas_vect[i,j] =  self.largest_betas[j]
+        #         else:
+        #             class_betas_vect[i,j] = 0
+        # return class_alphas_vect, class_betas_vect
+    
+    def classify(self):
+        class_coefficients_sum = self.class_coefs_vect()
+        
+        # coefficients_sum = class_alphas_vect + class_betas_vect
+        coefs_sums = [np.sum(class_coefficients_sum[i]) for i in range(len(class_coefficients_sum))]
+        return coefs_sums.index(max(coefs_sums))
+
+class polynomial_EkNN_R:
+    def __init__(self, X, y, alphas, betas, x_labels_encode, k):
+        self.alphas = alphas 
+        self.betas = betas
+        self.k = k
+        self.X = X
+        self.y = y
+        self.x_labels_encode = x_labels_encode # 0,1 vector
+        
+        # generate k largest coefficients for the model
+        sum_coefs = alphas + betas
+        sum_coefs_list = sum_coefs.tolist()
+        sum_coefs_list.sort(reverse=True)
+        max_sum_coefs_list = sum_coefs_list[0:self.k]
+        
+        k_largest_sum_coefs = alphas + betas
+        
+        for j in range(len(sum_coefs_list)):
+            if k_largest_sum_coefs[j] not in max_sum_coefs_list:
+                k_largest_sum_coefs[j] = 0
+        
+        for i in range(len(sum_coefs_list)):
+            if k_largest_sum_coefs[i] == 0:
+                alphas[i] = 0
+                betas[i] = 0
+        
+        self.largest_alphas = alphas
+        self.largest_betas = betas
+        self.k_largest_sum_coefs = k_largest_sum_coefs
+
+    def class_coefs_vect(self):
+        
+        class_coefs_vect = np.zeros((self.x_labels_encode.shape[0],self.X.shape[1])) # m classes x n number of observations 
+        
+        for i in range(len(self.x_labels_encode)):
+            for j in range(self.X.shape[1]):
+                if self.x_labels_encode[i, j] == 1:
+                    class_coefs_vect[i, j] = self.k_largest_sum_coefs[j]
+                else:
+                    class_coefs_vect[i, i] = 0
+                    
+        # class_betas_vect = np.zeros((len(self.x_labels_encode.shape[0]), self.X.shape[1]))
+        
+        # for i in range(len(self.x_labels_encode)):
+        #     for j in range(len(self.X.shape[1])):
+        #         if self.x_labels_encode[i,j] == 1:
+        #             class_betas_vect[i,j] =  self.largest_betas[j]
+        #         else:
+        #             class_betas_vect[i,j] = 0
+        return class_coefs_vect               
+                
+    # def create_obs_vects(self):
+    #     return np.transpose(self.X) # return nxp matrix
+
+        
+    def distance(self):
+        x_vect = np.transpose(self.X)
+        # print(x_vect.shape)
+        d_vect = np.zeros(self.X.shape[1]) # number of observations
+        
+        for i in range(self.X.shape[1]):
+            if self.k_largest_sum_coefs[i] == 0:
+                d_vect[i] = 0 # is it safe to set those not k largest to 0
+            else: 
+                d_vect[i] = np.linalg.norm(self.y + (self.y**2) - (self.alphas[i] * x_vect[i,:] + self.betas[i] * (x_vect[i,:] ** 2)), ord=2)
+                
+        return d_vect
+
+    def weights(self):
+        d_vect = self.distance()
+        weights = np.zeros(d_vect.shape[0])
+        
+        for i in range(self.X.shape[1]):
+            if self.k_largest_sum_coefs[i] == 0:  # not one of k obs with the largest coefficients
+                weights[i] = 0
+            elif np.amax(d_vect) != np.amin(d_vect):
+                weights[i] = ((np.amax(d_vect) - d_vect[i])/(np.amax(d_vect)-np.amin(d_vect)))
+            else:
+                weights[i] = 1
+        return weights
+    
+    def classify(self):
+        class_coefs_vect = self.class_coefs_vect()
+        weights_coefs_product = class_coefs_vect @ self.weights()
+        print(weights_coefs_product, weights_coefs_product.shape)
+        return np.where(weights_coefs_product == max(weights_coefs_product))[0][0]
+            
+
+class polynomial_EkNN(BaseEstimator, ClassifierMixin):
+    def __init__(self, lambda_=1.0,  group_num=1, k=3):
+        # self.lambda_1 = lambda_1
+        self.lambda_ = lambda_
+        self.group_num = group_num
+        self.k = k
+    
+    def fit(self, X_train, y_train):
+        self.group_vect = self.group_encode(X_train)
+        self.X_train = np.transpose(X_train)
+        class_names = [i for i in range(len(np.unique(y_train)))]
+        
+        self.y_train = self.label_matrix(class_names, y_train)
+        self.is_fitted_ = True
+        return self
+        
+    def group_encode(self, X): #
+        # sth
+        kmeans= KMeans(n_clusters = self.group_num, random_state=0, n_init="auto").fit(X)
+        result = kmeans.labels_
+        group_vect = np.zeros((self.group_num, len(result)))
+        for i in range(self.group_num):
+            for j in range(len(result)):
+                if result[j] == i:
+                    # print(result[j],"and ",i )
+                    group_vect[i,j] = 1
+        # print (group_vect.shape)
+        return group_vect
+
+        
+        
+    def label_matrix(self, label_names:list, labels):
+        """_summary_
+
+        Args:
+            label_names (list): _description_
+            labels (_numpy array_): nx1 matrix containing labels of the training observations
+
+        Returns:
+            _type_: _description_
+        """
+        m = len(label_names) # number of class labels
+ 
+        n = labels.shape[0] # number of training observations
+        label_mat = np.zeros((m, n))
+        
+        for i in range(m):
+            for ind, j in enumerate(labels):
+                if j == label_names[i]:
+                    label_mat[i,ind] = 1
+                else:
+                    label_mat[i,ind] = 0
+                    
+        return label_mat
+
+    def predict(self, X_test):
+        # create predictions result
+        preds = np.zeros(X_test.shape[1]) # number of testing observations
+        
+        # n = self.X_train.shape[1] # number of training observations
+        print(self.X_train.shape[1], type(self.X_train.shape[1]))
+        
+        # reshape test sample Y
+        y_s = np.transpose(X_test) # y_s = nxp, y_s[i] = (p,)
+        
+        # find alphas hat and beta hat
+        for i in range(y_s.shape[0]):
+            # xL2 = np.random.rand((self.X.shape[1]))
+            
+            # print((np.linalg.pinv(self.X_train).shape), y_s[i].shape)
+            alphas_0 = np.linalg.pinv(self.X_train) @ y_s[i]
+            betas_0 = (np.linalg.pinv(self.X_train) ** 2) @ y_s[i]
+            x0 = np.concatenate((alphas_0, betas_0), axis=0)
+            print(x0)
+            reg = polynomial_reg(self.X_train, y_s[i], self.group_vect, self.lambda_)
+            coefs = reg.lasso_optimize(x0).x
+            # print(reg.lasso_optimize(xL2).message)
+            print(coefs)
+            poly_EkNN_R = polynomial_EkNN_R(self.X_train, y_s[i], coefs[0:self.X_train.shape[1]], coefs[self.X_train.shape[1]:], self.y_train, self.k)
+            preds[i] = poly_EkNN_R.classify()
+
+        return preds
+        
+        
+        
+    def score(self, Y, y_labels):
+        """
+        Calculate the accuracy of the classifier.
+        """
+        y_pred = self.predict(Y)
+        return accuracy_score(y_labels, y_pred)  
+            
+        
